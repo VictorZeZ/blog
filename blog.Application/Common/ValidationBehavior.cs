694ab2313b0b3e020b475c5cefa3fa3f647f65e6
Meraj Esmaeili <merajesmaeili492@gmail.com>
2026-06-25 13:59:34 +0330

    Add MediatR, FluentValidation, and validation pipeline
    Introduce application layer setup with MediatR and FluentValidation.
    Register handlers, validators, and ValidationBehavior for request validation.
    Update Program.cs to add application services and add a project reference to blog.Domain.

--- blog.Application/Common/ValidationBehavior.cs
+++ blog.Application/Common/ValidationBehavior.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FluentValidation;
+using MediatR;
+using ValidationException = blog.Domain.Exceptions.ValidationException;
 
 namespace blog.Application.Common
 {
-    internal class ValidationBehavior
+    public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
     {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (!validators.Any())
+                return await next();
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var errors = validators
+                .Select(v => v.Validate(context))
+                .SelectMany(r => r.Errors)
+                .Where(e => e is not null)
+                .Select(e => new { Field = e.PropertyName, Reason = e.ErrorMessage })
+                .ToList();
+
+            if (errors.Count != 0)
+                throw new ValidationException(errors);
+
+            return await next();
+        }
     }
 }
