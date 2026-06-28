4a9b936ea03caf19df5bf5b21d5865c2bf6509db
Meraj Esmaeili <merajesmaeili492@gmail.com>
2026-06-28 17:15:10 +0330

    refactor(auth): move refresh token commands to Users namespace
    - Move RefreshToken command, handler, validator, and response from Tokens to Users
    - Update namespaces to maintain consistency with other auth-related user commands

--- blog.Application/Users/Commands/RefreshToken/RefreshTokenCommand.cs
+++ blog.Application/Users/Commands/RefreshToken/RefreshTokenCommand.cs
@@ -1,6 +1,6 @@
 ﻿using MediatR;
 
-namespace blog.Application.Tokens.Commands.RefreshToken
+namespace blog.Application.Users.Commands.RefreshToken
 {
     public class RefreshTokenCommand : IRequest<RefreshTokenResponse>
     {
