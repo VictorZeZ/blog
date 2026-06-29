a5035191066ba39e5ba6ee36ce96c6873e3a8b01
Meraj Esmaeili <merajesmaeili492@gmail.com>
2026-06-29 15:29:56 +0330

    Add AdminController with user management endpoints
    Added AdminController with endpoints for listing users, banning/unbanning users, and changing user levels using MediatR. Introduced BanUserRequest and ChangeUserLevelRequest DTOs for request handling.

---
+++ blog.Api/Controllers/AdminController.cs
@@ -0,0 +1,62 @@
+﻿using blog.Api.Common;
+using blog.Api.DTOs.Users;
+using blog.Application.Users.Commands.BanUser;
+using blog.Application.Users.Commands.ChangeUserLevel;
+using blog.Application.Users.Queries.GetUsers;
+using blog.Domain.Common;
+using blog.Domain.Users.Enums;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AdminController(IMediator mediator) : ApiController(mediator)
+    {
+        [HttpGet("users")]
+        public async Task<IActionResult> GetUsers([FromQuery] PagedRequest paging, [FromQuery] UserSortBy sortBy = UserSortBy.Newest, [FromQuery] UserFilter filter = UserFilter.All, CancellationToken ct = default)
+        {
+            var query = new GetUsersQuery
+            {
+                ActorId = CurrentUserId,
+                Paging = paging,
+                SortBy = sortBy,
+                Filter = filter
+            };
+
+            var result = await Mediator.Send(query, ct);
+            return Ok(result);
+        }
+
+        [HttpPut("users/{targetUserId:guid}/ban")]
+        public async Task<IActionResult> BanUser(Guid targetUserId, [FromBody] BanUserRequest request, CancellationToken ct)
+        {
+            var command = new BanUserCommand
+            {
+                ActorId = CurrentUserId,
+                TargetUserId = targetUserId,
+                IsBanned = request.IsBanned
+            };
+
+            var result = await Mediator.Send(command, ct);
+            return Ok(result);
+        }
+
+        [HttpPut("users/{targetUserId:guid}/level")]
+        public async Task<IActionResult> ChangeUserLevel(Guid targetUserId, [FromBody] ChangeUserLevelRequest request, CancellationToken ct)
+        {
+            var command = new ChangeUserLevelCommand
+            {
+                ActorId = CurrentUserId,
+                TargetUserId = targetUserId,
+                Level = request.Level
+            };
+
+            var result = await Mediator.Send(command, ct);
+            return Ok(result);
+        }
+    }
+}
---
+++ blog.Api/DTOs/Users/ChangeUserLevelRequest.cs
@@ -0,0 +1,9 @@
+﻿using blog.Domain.Users.Enums;
+
+namespace blog.Api.DTOs.Users
+{
+    public class ChangeUserLevelRequest
+    {
+        public UserLevel Level { get; init; }
+    }
+}
