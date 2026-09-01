5382f747b0f127aa14c46f7d255f08e48bf49700
Meraj Esmaeili <merajesmaeili492@gmail.com>
2026-09-01 14:45:44 +0330

    Add DashboardController with MediatR-based GET endpoint
    Added DashboardController secured by authorization. Implements a GET endpoint for dashboard data using MediatR, sending a GetDashboardQuery with the current user's ID and returning the result.

---
+++ blog.Api/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using blog.Api.Common;
+using blog.Application.Dashboard.Queries.GetDashboard;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blog.Api.Controllers
+{
+    [Authorize]
+    public class DashboardController(IMediator mediator) : ApiController(mediator)
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetDashboard(CancellationToken ct)
+        {
+            var query = new GetDashboardQuery { ActorId = CurrentUserId };
+
+            var result = await Mediator.Send(query, ct);
+            return Ok(result);
+        }
+    }
+}
