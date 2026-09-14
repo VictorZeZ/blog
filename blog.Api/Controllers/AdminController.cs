ad1a8868e5304bb9ab216b4cb58b521b6d0646e3
Meraj Esmaeili <merajesmaeili492@gmail.com>
2026-09-15 00:42:53 +0330

    Refactor report endpoints; update resource configs
    Moved report/statistics endpoints from AdminController to DashboardController with related using statements. Updated resources.yaml for blog.Api endpoint info and added workspace.globals.yaml with empty values.

--- blog.Api/Controllers/AdminController.cs
+++ blog.Api/Controllers/AdminController.cs
@@ -1,22 +1,13 @@
 ﻿using blog.Api.Common;
 using blog.Api.DTOs.Posts;
 using blog.Api.DTOs.Users;
-using blog.Application.Categories.Queries.GetCategoryPerformanceReport;
 using blog.Application.Posts.Commands.ChangePostStatus;
 using blog.Application.Posts.Queries.GetAllPosts;
-using blog.Application.Posts.Queries.GetCategoryPostStatusReport;
 using blog.Application.Posts.Queries.GetPendingApprovalPosts;
-using blog.Application.Posts.Queries.GetPostsReport;
-using blog.Application.Posts.Queries.GetPostStatusReport;
-using blog.Application.Posts.Queries.GetTopPosts;
-using blog.Application.Posts.Queries.GetUserPostStatusReport;
 using blog.Application.Users.Commands.BanUser;
 using blog.Application.Users.Commands.ChangeUserLevel;
-using blog.Application.Users.Queries.GetTopAuthors;
-using blog.Application.Users.Queries.GetUserActivityReport;
 using blog.Application.Users.Queries.GetUsers;
 using blog.Domain.Common;
-using blog.Domain.Common.Reports;
 using blog.Domain.Posts.Enums;
 using blog.Domain.Users.Enums;
 using MediatR;
@@ -111,128 +102,6 @@ namespace blog.Api.Controllers
             };
 
             var result = await Mediator.Send(command, ct);
-            return Ok(result);
-        }
-
-        [HttpGet("posts/status-report")]
-        public async Task<IActionResult> GetPostStatusReport([FromQuery] DateOnly? from, [FromQuery] DateOnly? to, CancellationToken ct)
-        {
-            var query = new GetPostStatusReportQuery
-            {
-                ActorId = CurrentUserId,
-                From = from,
-                To = to
-            };
-
-            var result = await Mediator.Send(query, ct);
-            return Ok(result);
-        }
-
-        [HttpGet("posts/{authorId:guid}/status-report")]
-        public async Task<IActionResult> GetUserPostStatusReport(Guid authorId, [FromQuery] DateOnly? from, [FromQuery] DateOnly? to, CancellationToken ct)
-        {
-            var query = new GetUserPostStatusReportQuery
-            {
-                ActorId = CurrentUserId,
-                AuthorId = authorId,
-                From = from,
-                To = to
-            };
-
-            var result = await Mediator.Send(query, ct);
-            return Ok(result);
-        }
-
-        [HttpGet("categories/{categoryId:guid}/status-report")]
-        public async Task<IActionResult> GetCategoryPostStatusReport(Guid categoryId, [FromQuery] DateOnly? from, [FromQuery] DateOnly? to, CancellationToken ct)
-        {
-            var query = new GetCategoryPostStatusReportQuery
-            {
-                ActorId = CurrentUserId,
-                CategoryId = categoryId,
-                From = from,
-                To = to
-            };
-
-            var result = await Mediator.Send(query, ct);
-            return Ok(result);
-        }
-
-        [HttpGet("categories/performance-report")]
-        public async Task<IActionResult> GetCategoryPerformanceReport([FromQuery] DateOnly? from, [FromQuery] DateOnly? to, CancellationToken ct)
-        {
-            var query = new GetCategoryPerformanceReportQuery
-            {
-                ActorId = CurrentUserId,
-                From = from,
-                To = to
-            };
-
-            var result = await Mediator.Send(query, ct);
-            return Ok(result);
-        }
-
-        [HttpGet("posts/report")]
-        public async Task<IActionResult> GetPostsReport([FromQuery] PagedRequest paging, [FromQuery] DateOnly? from, [FromQuery] DateOnly? to, [FromQuery] PostFilter filter = PostFilter.All, [FromQuery] PostSortBy sortBy = PostSortBy.Newest, [FromQuery] Guid? categoryId = null, [FromQuery] Guid? authorId = null, CancellationToken ct = default)
-        {
-            var query = new GetPostsReportQuery
-            {
-                ActorId = CurrentUserId,
-                Paging = paging,
-                From = from,
-                To = to,
-                Filter = filter,
-                SortBy = sortBy,
-                CategoryId = categoryId,
-                AuthorId = authorId
-            };
-
-            var result = await Mediator.Send(query, ct);
-            return Ok(result);
-        }
-
-        [HttpGet("posts/top")]
-        public async Task<IActionResult> GetTopPosts([FromQuery] DateOnly? from, [FromQuery] DateOnly? to, [FromQuery] int topN = TopNRules.DefaultTopN, [FromQuery] Guid? categoryId = null, CancellationToken ct = default)
-        {
-            var query = new GetTopPostsQuery
-            {
-                ActorId = CurrentUserId,
-                From = from,
-                To = to,
-                TopN = topN,
-                CategoryId = categoryId
-            };
-
-            var result = await Mediator.Send(query, ct);
-            return Ok(result);
-        }
-
-        [HttpGet("users/top-authors")]
-        public async Task<IActionResult> GetTopAuthors([FromQuery] DateOnly? from, [FromQuery] DateOnly? to, [FromQuery] int topN = TopNRules.DefaultTopN, CancellationToken ct = default)
-        {
-            var query = new GetTopAuthorsQuery
-            {
-                ActorId = CurrentUserId,
-                From = from,
-                To = to,
-                TopN = topN
-            };
-
-            var result = await Mediator.Send(query, ct);
-            return Ok(result);
-        }
-
-        [HttpGet("users/activity-report")]
-        public async Task<IActionResult> GetUserActivityReport([FromQuery] DateOnly? from, [FromQuery] DateOnly? to, CancellationToken ct)
-        {
-            var query = new GetUserActivityReportQuery
-            {
-                ActorId = CurrentUserId,
-                From = from,
-                To = to
-            };
-
-            var result = await Mediator.Send(query, ct);
             return Ok(result);
         }
     }
--- blog.Api/Controllers/DashboardController.cs
+++ blog.Api/Controllers/DashboardController.cs
@@ -1,5 +1,16 @@
 ﻿using blog.Api.Common;
+using blog.Application.Categories.Queries.GetCategoryPerformanceReport;
 using blog.Application.Dashboard.Queries.GetDashboard;
+using blog.Application.Posts.Queries.GetCategoryPostStatusReport;
+using blog.Application.Posts.Queries.GetPostsReport;
+using blog.Application.Posts.Queries.GetPostStatusReport;
+using blog.Application.Posts.Queries.GetTopPosts;
+using blog.Application.Posts.Queries.GetUserPostStatusReport;
+using blog.Application.Users.Queries.GetTopAuthors;
+using blog.Application.Users.Queries.GetUserActivityReport;
+using blog.Domain.Common;
+using blog.Domain.Common.Reports;
+using blog.Domain.Posts.Enums;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +24,128 @@ namespace blog.Api.Controllers
         public async Task<IActionResult> GetDashboard(CancellationToken ct)
         {
             var query = new GetDashboardQuery { ActorId = CurrentUserId };
+
+            var result = await Mediator.Send(query, ct);
+            return Ok(result);
+        }
+
+        [HttpGet("posts/status-report")]
+        public async Task<IActionResult> GetPostStatusReport([FromQuery] DateOnly? from, [FromQuery] DateOnly? to, CancellationToken ct)
+        {
+            var query = new GetPostStatusReportQuery
+            {
+                ActorId = CurrentUserId,
+                From = from,
+                To = to
+            };
+
+            var result = await Mediator.Send(query, ct);
+            return Ok(result);
+        }
+
+        [HttpGet("posts/{authorId:guid}/status-report")]
+        public async Task<IActionResult> GetUserPostStatusReport(Guid authorId, [FromQuery] DateOnly? from, [FromQuery] DateOnly? to, CancellationToken ct)
+        {
+            var query = new GetUserPostStatusReportQuery
+            {
+                ActorId = CurrentUserId,
+                AuthorId = authorId,
+                From = from,
+                To = to
+            };
+
+            var result = await Mediator.Send(query, ct);
+            return Ok(result);
+        }
+
+        [HttpGet("categories/{categoryId:guid}/status-report")]
+        public async Task<IActionResult> GetCategoryPostStatusReport(Guid categoryId, [FromQuery] DateOnly? from, [FromQuery] DateOnly? to, CancellationToken ct)
+        {
+            var query = new GetCategoryPostStatusReportQuery
+            {
+                ActorId = CurrentUserId,
+                CategoryId = categoryId,
+                From = from,
+                To = to
+            };
+
+            var result = await Mediator.Send(query, ct);
+            return Ok(result);
+        }
+
+        [HttpGet("categories/performance-report")]
+        public async Task<IActionResult> GetCategoryPerformanceReport([FromQuery] DateOnly? from, [FromQuery] DateOnly? to, CancellationToken ct)
+        {
+            var query = new GetCategoryPerformanceReportQuery
+            {
+                ActorId = CurrentUserId,
+                From = from,
+                To = to
+            };
+
+            var result = await Mediator.Send(query, ct);
+            return Ok(result);
+        }
+
+        [HttpGet("posts/report")]
+        public async Task<IActionResult> GetPostsReport([FromQuery] PagedRequest paging, [FromQuery] DateOnly? from, [FromQuery] DateOnly? to, [FromQuery] PostFilter filter = PostFilter.All, [FromQuery] PostSortBy sortBy = PostSortBy.Newest, [FromQuery] Guid? categoryId = null, [FromQuery] Guid? authorId = null, CancellationToken ct = default)
+        {
+            var query = new GetPostsReportQuery
+            {
+                ActorId = CurrentUserId,
+                Paging = paging,
+                From = from,
+                To = to,
+                Filter = filter,
+                SortBy = sortBy,
+                CategoryId = categoryId,
+                AuthorId = authorId
+            };
+
+            var result = await Mediator.Send(query, ct);
+            return Ok(result);
+        }
+
+        [HttpGet("posts/top")]
+        public async Task<IActionResult> GetTopPosts([FromQuery] DateOnly? from, [FromQuery] DateOnly? to, [FromQuery] int topN = TopNRules.DefaultTopN, [FromQuery] Guid? categoryId = null, CancellationToken ct = default)
+        {
+            var query = new GetTopPostsQuery
+            {
+                ActorId = CurrentUserId,
+                From = from,
+                To = to,
+                TopN = topN,
+                CategoryId = categoryId
+            };
+
+            var result = await Mediator.Send(query, ct);
+            return Ok(result);
+        }
+
+        [HttpGet("users/top-authors")]
+        public async Task<IActionResult> GetTopAuthors([FromQuery] DateOnly? from, [FromQuery] DateOnly? to, [FromQuery] int topN = TopNRules.DefaultTopN, CancellationToken ct = default)
+        {
+            var query = new GetTopAuthorsQuery
+            {
+                ActorId = CurrentUserId,
+                From = from,
+                To = to,
+                TopN = topN
+            };
+
+            var result = await Mediator.Send(query, ct);
+            return Ok(result);
+        }
+
+        [HttpGet("users/activity-report")]
+        public async Task<IActionResult> GetUserActivityReport([FromQuery] DateOnly? from, [FromQuery] DateOnly? to, CancellationToken ct)
+        {
+            var query = new GetUserActivityReportQuery
+            {
+                ActorId = CurrentUserId,
+                From = from,
+                To = to
+            };
 
             var result = await Mediator.Send(query, ct);
             return Ok(result);
